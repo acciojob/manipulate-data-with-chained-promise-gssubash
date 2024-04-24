b709d6c7a9b6d03e589ffb27df4586d45343448b
Subash Ganesan <74418430+gssubash@users.noreply.github.com>
2024-04-24 07:02:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,16 +4,17 @@ const outputContainer = document.getElementById('output');
 let arr = [1,2,3,4]; 
 const pr  = new Promise((resolve,reject)=>{
 	
-		resolve(arr);  
+		resolve(arr);   
 	 
 });
 
 pr.then((data)=>{
 	return new Promise((rs,rj)=>{
-		setTimeout(()=>{
+		setTimeout(()=>{ 
 		let val =data.filter(a=>a%2==0);
+			outputContainer.innerText = val;
 	rs(val); 
-	},1000); 
+	},1000);  
 	})
 	
 }).then((data)=>{
